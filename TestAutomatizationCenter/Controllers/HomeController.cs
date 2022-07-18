096bc4cb21a1a66a4721ce5831d100e77ed2f6ae
SuperNoice <SuperNoice@yandex.ru>
2022-07-18 16:32:52 +0300

    Added registration validation. Added error handling.

--- TestAutomatizationCenter/Controllers/HomeController.cs
+++ TestAutomatizationCenter/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using TestAutomatizationCenter.Models;
-using System.Security.Cryptography;
-using System.Text;
+using TestAutomatizationCenter.Utils;
 
 namespace TestAutomatizationCenter.Controllers
 {
@@ -24,70 +23,106 @@ namespace TestAutomatizationCenter.Controllers
 
         public IActionResult Index()
         {
-            return View(_db.Users.ToList());
+            try
+            {
+                return View(_db.Users.ToList());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Index: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         public IActionResult Chat()
         {
-            var chatContent = new ChatContent();
-
-            foreach(var user in _db.Users.ToArray())
+            try
             {
-                chatContent.Users.Add(user.Login);
+                var chatContent = new ChatContent();
+
+                foreach (var user in _db.Users.ToArray())
+                {
+                    chatContent.Users.Add(user.Login);
+                }
+
+                chatContent.Messages.AddRange(_db.Messages.ToArray());
+
+                return View(chatContent);
             }
-
-            chatContent.Messages.AddRange(_db.Messages.ToArray());
-
-            return View(chatContent);
-        }
-
-        private static string GetHashSha256(string text)
-        {
-            byte[] bytes = Encoding.Unicode.GetBytes(text);
-            SHA256Managed hashstring = new SHA256Managed();
-            byte[] hash = hashstring.ComputeHash(bytes);
-            string hashString = string.Empty;
-            foreach (byte x in hash)
+            catch (Exception ex)
             {
-                hashString += string.Format("{0:x2}", x);
+                _logger.LogError(ex, "Chat: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            return hashString;
         }
 
         public async Task<IActionResult> AddUser(User user)
         {
-            user.Password = GetHashSha256(user.Password);
-            await _db.Users.AddAsync(user);
-            await _db.SaveChangesAsync();
+            try
+            {
+                user.Name = user.Name.Trim();
+                user.Login = user.Login.Trim();
+                user.Password = user.Password.Trim();
 
-            return RedirectToAction("Index");
+                if (user.Name == "" || user.Login == "" || user.Password == "")
+                {
+                    return BadRequest();
+                }
+
+                user.Password = Cryptography.GetHashSha256(user.Password);
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "AddUser: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         public async Task<IActionResult> SendMessage(string text, string login)
         {
-            var user = _db.Users.FirstOrDefault(x => x.Login == login);
-            
-            if (user == null)
+            try
             {
-                return BadRequest();
+                var user = _db.Users.FirstOrDefault(x => x.Login == login);
+
+                if (user == null)
+                {
+                    return BadRequest();
+                }
+
+                var message = new Message()
+                {
+                    Text = text,
+                    TimeStamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                    User = user
+                };
+
+                await _db.Messages.AddAsync(message);
+                await _db.SaveChangesAsync();
+
+                return Ok(message);
             }
-
-            var message = new Message()
+            catch (Exception ex)
             {
-                Text = text,
-                TimeStamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                User = user
-            };
-
-            await _db.Messages.AddAsync(message);
-            await _db.SaveChangesAsync();
-
-            return Ok(message);
+                _logger.LogError(ex, "SendMessage: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         public IActionResult GetMessages()
         {
-            return Ok(_db.Messages.ToArray());
+            try
+            {
+                return Ok(_db.Messages.ToArray());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "GetMessages: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TestAutomatizationCenter/Utils/Cryptography.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TestAutomatizationCenter.Utils
+{
+    public class Cryptography
+    {
+        public static string GetHashSha256(string text)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += string.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
